7d02ce3aba9467ed5f6a5150c6cc3e7cad49c21f
Gogs <gogs@fake.local>
2021-07-31 13:45:07 -0300

    tomcat deploy

--- src/main/java/br/edu/infnet/bootalunoapp/BootalunoappApplication.java
+++ src/main/java/br/edu/infnet/bootalunoapp/BootalunoappApplication.java
@@ -2,9 +2,10 @@ package br.edu.infnet.bootalunoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BootalunoappApplication {
+public class BootalunoappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootalunoappApplication.class, args);
